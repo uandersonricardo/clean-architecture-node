4920244be13fccfcd6903b80ff8904cee5a0a1ed
Uanderson Ricardo <uandersonrfs@gmail.com>
2021-10-28 00:06:46 -0300

    feat: crypt passwords on sign up

--- src/presentation/controllers/AuthController.ts
+++ src/presentation/controllers/AuthController.ts
@@ -1,4 +1,5 @@
 import SignUp from "@application/useCases/auth/SignUp";
+import BCryptManager from "@infrastructure/gateways/BCryptManager";
 import NanoIdGenerator from "@infrastructure/gateways/NanoIdGenerator";
 import UserRepositoryInMemory from "@infrastructure/repositories/inMemory/UserRepositoryInMemory";
 
@@ -6,8 +7,13 @@ export default class UserController {
   static async signUp(params: any, body: any) {
     const userRepositoryInMemory = new UserRepositoryInMemory();
     const nanoIdGenerator = new NanoIdGenerator();
+    const bcryptManager = new BCryptManager();
 
-    const signUp = new SignUp(userRepositoryInMemory, nanoIdGenerator);
+    const signUp = new SignUp(
+      userRepositoryInMemory,
+      nanoIdGenerator,
+      bcryptManager
+    );
     const userId = await signUp.execute(body);
 
     return userId;
